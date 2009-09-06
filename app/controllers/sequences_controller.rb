27bb9af2118b4f8256871c3cb06efa9f806b7ca1
Ben Sales <bensales@b2climbing.com>
2009-09-06 17:22:13 +0100

    Allow addition of exercises to a sequence.

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -10,25 +10,26 @@ class SequencesController < ApplicationController
 
   def edit
     @sequence = Sequence.find(params[:id])
+    @exercises = Exercise.all
   end
-  
+
   def show
     @sequence = Sequence.find(params[:id])
   end
-  
+
   def update
     @sequence = Sequence.find(params[:id])
     @sequence.update_attributes!(params[:sequence])
     flash[:notice] = 'Sequence updated'
     redirect_to :action => :edit
   end
-  
+
   def create
     @sequence = Sequence.new(params[:sequence])
     if @sequence.valid?
       @sequence.save
       flash[:notice] = 'Sequence created'
-      redirect_to :action => :index
+      redirect_to :action => :edit, :id => @sequence.id
     else
       render :action => :new
     end
@@ -39,6 +40,15 @@ class SequencesController < ApplicationController
       SequenceExercise.find(order.to_i).update_attribute(:exercise_order, index)
     end
     render :nothing => true
+  end
+
+  def add_exercise
+    sequence_exercise = SequenceExercise.new(:sequence_id => params[:id], :exercise_id => params[:exercise])
+    sequence_exercise.save
+    @sequence = Sequence.find(params[:id])
+    render :update do |page|
+      page.replace_html 'exercise-list', :partial => 'exercise_list'
+    end
   end
 
 end
